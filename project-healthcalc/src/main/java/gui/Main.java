63d75e84429d95509813c75d736a2d81337d0e68
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-04-01 18:04:56 +0200

    Feature: Añadido main gui

---
+++ project-healthcalc/src/main/java/gui/Main.java
@@ -0,0 +1,25 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import healthcalc.HealthCalcImpl;
+
+
+public class Main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HealthCalcImpl modelo = new HealthCalcImpl();
+					Vista vista = new Vista();
+					Controlador controlador = new Controlador(modelo, vista);
+					vista.registrarControlador(controlador);
+					
+					vista.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
