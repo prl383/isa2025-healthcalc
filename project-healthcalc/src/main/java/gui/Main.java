732f94408561dd5cc78720a8035799d3480c46b4
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-05-06 13:29:01 +0200

    Feature: Aplicar patron Singleton a HealthCalc

--- project-healthcalc/src/main/java/gui/Main.java
+++ project-healthcalc/src/main/java/gui/Main.java
@@ -2,17 +2,18 @@ package gui;
 
 import java.awt.EventQueue;
 
+import healthcalc.HealthCalc;
 import healthcalc.HealthCalcImpl;
-
+import gui.Vista;
 
 public class Main {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HealthCalcImpl modelo = new HealthCalcImpl();
+					HealthCalcImpl c = HealthCalcImpl.getInstance();
 					Vista vista = new Vista();
-					Controlador controlador = new Controlador(modelo, vista);
+					Controlador controlador = new Controlador(c, vista);
 					vista.registrarControlador(controlador);
 					
 					vista.setVisible(true);
