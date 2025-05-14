42cf912a7f04fc2f3f9fc735dfc22a723c623b09
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-05-14 18:13:11 +0200

    Fix: Arreglar Main para patrón Adapter

--- project-healthcalc/src/main/java/healthcalc/Main.java
+++ project-healthcalc/src/main/java/healthcalc/Main.java
@@ -2,16 +2,16 @@ package healthcalc;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        HealthCalc c = HealthCalcImpl.getInstance();
-        HealthHospital adapter = new HealthHospitalAdapter();
+        HealthCalcImpl c = HealthCalcImpl.getInstance();
+        HealthHospital adapter = new HealthHospitalAdapter(c);
+        //HealthStats statsProxy = new HealthStatsProxy(adapter);
         //float cal = c.idealWeight(195, 'm') ;
         //System.out.println(cal);          
         
-        float cal2 = c.basalMetabolicRate(98, 190, 40, 'm');
+        //float cal2 = c.basalMetabolicRate(98, 190, 40, 'm');
+        double cal2 = adapter.bmr('m', 40, 1.90f, 98000);
+
         System.out.println(cal2); 
-        
-        System.out.println("BMR adaptado: " + adapter.bmr('m', 40, 1.90f, 98000));
-        System.out.println("Peso ideal adaptado: " + adapter.pesoIdeal('m', 1.90f));
 
     }
 }
