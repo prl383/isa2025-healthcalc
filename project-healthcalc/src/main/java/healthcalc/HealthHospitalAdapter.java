a59595a0587510042dc6f8a784f1e602ad5d4d2c
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-05-15 10:59:50 +0200

    Fix: Arreglar constructor para patrón adapter

--- project-healthcalc/src/main/java/healthcalc/HealthHospitalAdapter.java
+++ project-healthcalc/src/main/java/healthcalc/HealthHospitalAdapter.java
@@ -3,23 +3,29 @@ package healthcalc;
 public class HealthHospitalAdapter implements HealthHospital {
      
     //hacer el constructor que le pase la calculadora (flecha blanca)
-    private final HealthCalcImpl calc;
-    public HealthHospitalAdapter(HealthCalcImpl calc) {
+    private final HealthCalc calc;
+    public HealthHospitalAdapter(HealthCalc calc) {
         this.calc = calc;
     }
     
 
     @Override
     public double bmr(char genero, int edad, float alturaMetros, int pesoGramos) {
-        float alturaCm = alturaMetros * 100;
-        float pesoKg = pesoGramos / 1000.0f;
+    float alturaCm = alturaMetros * 100;
+    float pesoKg = pesoGramos / 1000.0f;
+    try {
         return calc.basalMetabolicRate(pesoKg, (int) alturaCm, edad, genero);
+    } catch (Exception e) {       
+        return -1; 
     }
+}
 
-    @Override
     public int pesoIdeal(char genero, float alturaMetros) {
-        float alturaCm = alturaMetros * 100;
+    float alturaCm = alturaMetros * 100;
+    try {
         return Math.round(calc.idealWeight((int) alturaCm, genero));
+    } catch (Exception e) {
+        return -1;
+        }
     }
-    
 }
