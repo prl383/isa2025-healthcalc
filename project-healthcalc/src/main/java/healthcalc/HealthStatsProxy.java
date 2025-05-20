f151f00394fcda793aa12e4c01b81873315921c1
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-05-20 18:10:25 +0200

    Refactor: Utilizar Gender y Person

--- project-healthcalc/src/main/java/healthcalc/HealthStatsProxy.java
+++ project-healthcalc/src/main/java/healthcalc/HealthStatsProxy.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class HealthStatsProxy implements HealthStats, HealthCalc {
 
     private final HealthCalc calc;
+
     public HealthStatsProxy(HealthCalc calc) {
         this.calc = calc;
     }
@@ -14,33 +15,43 @@ public class HealthStatsProxy implements HealthStats, HealthCalc {
     private final List<Float> bmrResults = new ArrayList<>();
     private int usageCount = 0;
 
-        @Override
-    public float basalMetabolicRate(float weight, int height, int age, char gender) {
+    
+    @Override
+    public float idealWeight(Person person) {
         try {
-            float result = calc.basalMetabolicRate(weight, height, age, gender);
-            weights.add(weight);
-            bmrResults.add(result);
+            float result = calc.idealWeight(person);
             usageCount++;
             return result;
         } catch (Exception e) {
-            
-            return -1; 
+            return -1;
         }
     }
 
     @Override
-    public float idealWeight(int height, char gender) {
+    public float basalMetabolicRate(Person person) {
         try {
-            float result = calc.idealWeight(height, gender);
+            float result = calc.basalMetabolicRate(person);
+            weights.add(person.weight());
+            bmrResults.add(result);
             usageCount++;
             return result;
         } catch (Exception e) {
-            
-            return -1;  
+            return -1;
         }
     }
 
+    
+    public float idealWeight(int height, char gender) {
+        Person person = new BasicPerson(0f, height, 30, mapGender(gender)); // peso y edad de ejemplo
+        return idealWeight(person);
+    }
 
+    public float basalMetabolicRate(float weight, int height, int age, char gender) {
+        Person person = new BasicPerson(weight, height, age, mapGender(gender));
+        return basalMetabolicRate(person);
+    }
+
+    
     @Override
     public float getAvgWeight() {
         return (float) weights.stream().mapToDouble(Float::doubleValue).average().orElse(0);
@@ -54,5 +65,12 @@ public class HealthStatsProxy implements HealthStats, HealthCalc {
     @Override
     public int getUsageCount() {
         return usageCount;
+    }
+
+    
+    private Gender mapGender(char gender) {
+        if (gender == 'm') return Gender.MALE;
+        if (gender == 'w') return Gender.FEMALE;
+        throw new IllegalArgumentException("Género no válido: " + gender);
     }
 }
