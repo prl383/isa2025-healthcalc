234eb415ce7e7be41cde96e5fe2751f997938d82
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-05-20 18:10:07 +0200

    Refactor: Utilizar Gender y Person

--- project-healthcalc/src/main/java/healthcalc/HealthHospitalAdapter.java
+++ project-healthcalc/src/main/java/healthcalc/HealthHospitalAdapter.java
@@ -1,31 +1,41 @@
 package healthcalc;
 
 public class HealthHospitalAdapter implements HealthHospital {
-     
-    //hacer el constructor que le pase la calculadora (flecha blanca)
+
     private final HealthCalc calc;
+
     public HealthHospitalAdapter(HealthCalc calc) {
         this.calc = calc;
     }
-    
 
     @Override
     public double bmr(char genero, int edad, float alturaMetros, int pesoGramos) {
-    float alturaCm = alturaMetros * 100;
-    float pesoKg = pesoGramos / 1000.0f;
-    try {
-        return calc.basalMetabolicRate(pesoKg, (int) alturaCm, edad, genero);
-    } catch (Exception e) {       
-        return -1; 
-    }
-}
+        float alturaCm = alturaMetros * 100;
+        float pesoKg = pesoGramos / 1000.0f;
 
-    public int pesoIdeal(char genero, float alturaMetros) {
-    float alturaCm = alturaMetros * 100;
-    try {
-        return Math.round(calc.idealWeight((int) alturaCm, genero));
-    } catch (Exception e) {
-        return -1;
+        try {
+            Person person = new BasicPerson(pesoKg, alturaCm, edad, mapGender(genero));
+            return calc.basalMetabolicRate(person);
+        } catch (Exception e) {
+            return -1;
         }
+    }
+
+    @Override
+    public int pesoIdeal(char genero, float alturaMetros) {
+        float alturaCm = alturaMetros * 100;
+
+        try {
+            Person person = new BasicPerson(0, alturaCm, 30, mapGender(genero)); // peso/edad dummy
+            return Math.round(calc.idealWeight(person));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private Gender mapGender(char genderChar) {
+        if (genderChar == 'm') return Gender.MALE;
+        if (genderChar == 'w') return Gender.FEMALE;
+        throw new IllegalArgumentException("Género no válido: " + genderChar);
     }
 }
