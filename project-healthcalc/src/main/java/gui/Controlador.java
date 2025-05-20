4be5b0e2f17c1887e1c573f4955628b100bf95e6
Patricia Rodriguez Liduena <fuentesan.patri@gmail.com>
2025-05-20 18:08:19 +0200

    Refactor: Utilizar objetos Person y enum Gender para invocar la lógica del modelo

--- project-healthcalc/src/main/java/gui/Controlador.java
+++ project-healthcalc/src/main/java/gui/Controlador.java
@@ -3,51 +3,56 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import healthcalc.HealthCalcImpl;
-
-
-
+import healthcalc.BasicPerson;
+import healthcalc.Gender;
+import healthcalc.HealthCalc;
+import healthcalc.Person;
 
 public class Controlador implements ActionListener {
-	
-	private HealthCalcImpl modelo;
-	private Vista vista;
-	
-	public Controlador(HealthCalcImpl modelo, Vista vista) {
-		this.modelo = modelo;
-		this.vista = vista;
-	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		String comando = e.getActionCommand();
-		if(comando.equals("CALCULAR PI")) {
-			int a = vista.getAlturaPI();
-			char g = vista.getGeneroPI();
-			System.out.print(a);
-			System.out.print(g);
-			
-			try {
-				float resultado = modelo.idealWeight(a,g);	
-				System.out.println(resultado);
-				vista.setResutadoPI(resultado);
-				
-			} catch (IllegalArgumentException error) {
-				System.out.println("Introduce parámetros válidos.");
-			}
-		}else if(comando.equals("CALCULAR BMR")) {
-			int a = vista.getAlturaBMR();
-			float p = vista.getPesoBMR();
-			int e1 = vista.getEdad();
-			char g = vista.getGeneroBMR();
-			try {
-				float resultado = modelo.basalMetabolicRate(p,a,e1,g);	
-				vista.setResutadoBMR(resultado);
-			} catch (IllegalArgumentException error) {
-				System.out.println("Introduce parámetros válidos.");
-			}
-		}
-	}
+    private HealthCalc modelo;
+    private Vista vista;
 
+    public Controlador(HealthCalc modelo, Vista vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String comando = e.getActionCommand();
+
+        if (comando.equals("CALCULAR PI")) {
+            int altura = vista.getAlturaPI();
+            char generoChar = vista.getGeneroPI();
+
+            try {
+                Gender genero = (generoChar == 'm') ? Gender.MALE : Gender.FEMALE;
+                Person persona = new BasicPerson(0, altura, 0, genero); // peso y edad no son necesarios para PI
+
+                float resultado = modelo.idealWeight(persona);
+                vista.setResutadoPI(resultado);
+
+            } catch (IllegalArgumentException error) {
+                System.out.println("Introduce parámetros válidos.");
+            }
+
+        } else if (comando.equals("CALCULAR BMR")) {
+            int altura = vista.getAlturaBMR();
+            float peso = vista.getPesoBMR();
+            int edad = vista.getEdad();
+            char generoChar = vista.getGeneroBMR();
+
+            try {
+                Gender genero = (generoChar == 'm') ? Gender.MALE : Gender.FEMALE;
+                Person persona = new BasicPerson(peso, altura, edad, genero);
+
+                float resultado = modelo.basalMetabolicRate(persona);
+                vista.setResutadoBMR(resultado);
+
+            } catch (IllegalArgumentException error) {
+                System.out.println("Introduce parámetros válidos.");
+            }
+        }
+    }
 }
